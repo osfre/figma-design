0be6b868dbbf5c5a4a81c3defdf25f617b22b167
osfre <freedosama34@gmail.com>
2023-01-21 22:44:02 +0200

    new edit

--- js/main.js
+++ js/main.js
@@ -8,17 +8,20 @@
 
 let btn = document.querySelector(".btn-nav");
 let ulElement = document.querySelector(".nav-mobile-categories")
+let icon = document.querySelector(".btn-nav i.fa-solid.fa-bars");
 let exitnav = document.querySelector(".exitnav")
 
 
 btn.onclick = function () {
   ulElement.classList.add("show");
+  icon.classList.add("changecolor")
   opsit.classList.add("show-opsit");
 
 }
 exitnav.onclick = function () {
   ulElement.classList.remove("show");
   opsit.classList.remove("show-opsit");
+  icon.classList.remove("changecolor")
 }
 
 
@@ -26,16 +29,18 @@ exitnav.onclick = function () {
 let login_section = document.querySelector(".login-section");
 let btn_login = document.querySelector(".btn-login i");
 let opsit = document.querySelector(".opsity");
+let icon2 = document.querySelector(".btn-login i.fa-regular.fa-user");
 let exit_leftSide = document.querySelector(".exit");
-
 
 
 btn_login.onclick = () => {
   login_section.classList.add("show_login");
+  icon2.classList.add("changecolor2");
   opsit.classList.add("show-opsit");
 }
 exit_leftSide.onclick = () => {
   login_section.classList.remove("show_login");
+  icon2.classList.remove("changecolor2");
   opsit.classList.remove("show-opsit");
 }
 
